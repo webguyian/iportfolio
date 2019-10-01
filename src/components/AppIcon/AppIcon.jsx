1f7a5ab2dceb88476d5dd1d0e7504a80baffae34
Ian McPhail <webguyian@gmail.com>
2019-09-30 22:51:03 -0400

    Update AppIcon styles

--- src/components/AppIcon/AppIcon.jsx
+++ src/components/AppIcon/AppIcon.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import Button from 'components/Button/Button';
-import DateTime from 'components/DateTime/DateTime';
 import Text from 'components/Text/Text';
 
 class AppIcon extends Component {
   static propTypes = {
+    children: PropTypes.node,
     name: PropTypes.string
   };
 
@@ -16,31 +17,13 @@ class AppIcon extends Component {
     this.baseClass = 'ui-app-icon';
   }
 
-  get calendarIcon() {
-    const { name } = this.props;
-
-    return (
-      <Button className={this.baseClass}>
-        <Text className={`${this.baseClass}-weekday`}>
-          <DateTime format="dddd" />
-        </Text>
-        <Text className={`${this.baseClass}-day`}>
-          <DateTime format="D" />
-        </Text>
-        <Text className="ui-app-icon-label">{name}</Text>
-      </Button>
-    );
-  }
-
   render() {
-    const { name } = this.props;
-
-    if (name === 'Calendar') {
-      return this.calendarIcon;
-    }
+    const { children, name } = this.props;
+    const nameClass = name && `${this.baseClass}--${name.toLowerCase()}`;
 
     return (
-      <Button className={this.baseClass}>
+      <Button className={classNames(this.baseClass, nameClass)}>
+        {children}
         <Text className={`${this.baseClass}-label`}>{name}</Text>
       </Button>
     );
